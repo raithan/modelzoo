8cf5a53a388a7acd9c3fbfe02befcaa9aed66ab1
gaolj <gaolj@gaolj.com>
2025-06-12 18:23:20 +0800

    【25-Q2-生态建设】模型迁移-研发效能部-模型训练-在PyTorch框架上支持BertSum在CNN/DailyMail数据集上的训练

---
+++ PyTorch/NLP/BertSum/src/cuda_1card_test.sh
@@ -0,0 +1,21 @@
+python train.py \
+-mode train -encoder \
+transformer -dropout 0.1 \
+-bert_data_path ../bert_data/cnndm \
+-model_path ../models/bert_transformer \
+-lr 2e-3 \
+-visible_gpus 0  \
+-gpu_ranks 0 \
+-world_size 1 \
+-report_every 1 \
+-save_checkpoint_steps 1000 \
+-batch_size 3000 \
+-decay_method noam \
+-train_steps 100 \
+-accum_count 2 \
+-log_file ../logs/bert_transformer \
+-use_interval true \
+-warmup_steps 10000 \
+-ff_size 2048 \
+-inter_layers 2 \
+-heads 8 
