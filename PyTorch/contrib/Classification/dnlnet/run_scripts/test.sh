9cde8490322cb717b9ec49408834360026123573
yanyuan627 <147579262+yanyuan627@users.noreply.github.com>
2025-07-11 10:00:02 +0800

    Update test.sh

--- PyTorch/contrib/Classification/dnlnet/run_scripts/test.sh
+++ PyTorch/contrib/Classification/dnlnet/run_scripts/test.sh
@@ -16,7 +16,7 @@ pip3 install numpy==1.24.3
 cd $script_path
 
 
-python run_pspnet.py --config ../configs/pspnet/pspnet_r50-d8_4xb2-40k_cityscapes-512x1024 \
+python run_pspnet.py --config ../configs/pspnet/pspnet_r50-d8_4xb2-40k_cityscapes-512x1024.py \
        --launcher pytorch --nproc-per-node 1 --amp 2>&1 | tee sdaa.log
 # 生成loss对比图
 python loss.py --sdaa-log sdaa.log --cuda-log cuda.log
