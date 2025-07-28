408e1eaa2dfd3cb138a1eb994295e5a3a45833d9
asda8sdm <142647709+asda8sdm@users.noreply.github.com>
2025-07-28 16:39:17 +0800

    Update argument.py

--- PyTorch/contrib/Classification/resnet_cifar/run_scripts/argument.py
+++ PyTorch/contrib/Classification/resnet_cifar/run_scripts/argument.py
@@ -1,28 +1,21 @@
 import argparse
-import os
-import os.path as osp
-from copy import deepcopy
-
 from mmengine.config import Config, ConfigDict, DictAction
-from mmengine.utils import digit_version
-from mmengine.utils.dl_utils import TORCH_VERSION
-
-
 def parse_args():
-    """命令行参数解析"""
     parser = argparse.ArgumentParser(description='Train a model')
-    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume',
         nargs='?',
         type=str,
         const='auto',
-        help='If specify checkpoint path, resume from it, else auto resume from latest checkpoint')
+        help='If specify checkpoint path, resume from it, while if not '
+        'specify, try to auto resume from the latest checkpoint '
+        'in the work directory.')
     parser.add_argument(
         '--amp',
-        default='True',
         action='store_true',
+        default='True',
         help='enable automatic-mixed-precision training')
     parser.add_argument(
         '--no-validate',
@@ -31,30 +24,44 @@ def parse_args():
     parser.add_argument(
         '--auto-scale-lr',
         action='store_true',
-        help='whether to auto scale the learning rate according to actual batch size')
+        help='whether to auto scale the learning rate according to the '
+        'actual batch size and the original batch size.')
     parser.add_argument(
         '--no-pin-memory',
         action='store_true',
-        help='disable pin_memory in dataloaders')
+        help='whether to disable the pin_memory option in dataloaders.')
     parser.add_argument(
         '--no-persistent-workers',
         action='store_true',
-        help='disable persistent_workers in dataloaders')
+        help='whether to disable the persistent_workers option in dataloaders.'
+    )
     parser.add_argument(
         '--cfg-options',
         nargs='+',
         action=DictAction,
-        help='override some config settings, in xxx=yyy format')
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
+    # When using PyTorch version >= 2.0.0, the `torch.distributed.launch`
+    # will pass the `--local-rank` parameter to `tools/train.py` instead
+    # of `--local_rank`.
     parser.add_argument('--local_rank', '--local-rank', type=int, default=0)
-
+    parser.add_argument('--nnodes', type=int, default=1, help='number of nodes')
+    parser.add_argument('--nproc-per-node', type=int, default=1, help='number of gpus')
+    parser.add_argument('--master-addr', type=str, default='localhost')
+    parser.add_argument('--master-port', type=str, default='29500')
+    parser.add_argument('--node-rank',type=int,default='0',help='node rank')
     args = parser.parse_args()
-
-    if 'LOCAL_RANK' not in os.environ:
-        os.environ['LOCAL_RANK'] = str(args.local_rank)
-
+    
     return args
+
+    # if 'LOCAL_RANK' not in os.environ:
+    #     os.environ['LOCAL_RANK'] = str(args.local_rank)
