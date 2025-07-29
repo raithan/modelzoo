d38d6a9cd5809b7dfeda7844b16f22cae7a48f54
asda8sdm <142647709+asda8sdm@users.noreply.github.com>
2025-07-29 18:48:09 +0800

    Update cifar10_bs128.py

--- PyTorch/contrib/Classification/resnet_cifar/configs/_base_/schedules/cifar10_bs128.py
+++ PyTorch/contrib/Classification/resnet_cifar/configs/_base_/schedules/cifar10_bs128.py
@@ -1,10 +1,14 @@
-
-# optimizer
 optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001))
-# learning policy
+    loss_scale='dynamic',
+    optimizer=dict(lr=0.1, momentum=0.9, type='SGD', weight_decay=0.0001),
+    type='AmpOptimWrapper')
 param_scheduler = dict(
-    type='MultiStepLR', by_epoch=True, milestones=[100, 150], gamma=0.1)
+    by_epoch=True, gamma=0.1, milestones=[
+        100,
+        150,
+    ], type='MultiStepLR')
+# randomness = dict(deterministic=False, seed=None)
+# resume = False
 
 # train, val, test setting
 train_cfg = dict(by_epoch=True, max_epochs=200, val_interval=1)
@@ -14,4 +18,3 @@ test_cfg = dict()
 # NOTE: `auto_scale_lr` is for automatically scaling LR
 # based on the actual training batch size.
 auto_scale_lr = dict(base_batch_size=128)
-
