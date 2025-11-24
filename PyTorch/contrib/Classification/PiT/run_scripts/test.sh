6ce5d42c167997479d3468fe7fa2ba78c950a073
root <root@nb-nohjdtecps-0.nbc-rohvolzzwh.kubecube-workspace-606.svc.cluster.local>
2025-11-24 23:48:27 +0000

    “update”

---
+++ PyTorch/contrib/Classification/PiT/run_scripts/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+script_path=$(dirname $(readlink -f "$0"))
+echo "当前脚本路径: $script_path"
+cd .. 
+#安装依赖
+pip install -r requirements.txt
+echo "默认 1 个进程（1 GPU）；可以通过第一个参数覆盖，比如 ./test.sh 4"
+NUM_PROC=${1:-1}
+cd $script_path
+echo "Using NUM_PROC=${NUM_PROC}"
+echo "执行训练"
+torchrun --nproc_per_node=${NUM_PROC} train.py \
+  --data-dir /data/teco-data/imagenet/ \
+  --model pit_b_224 \
+  -b 128 \
+  --sched step \
+  --epochs 450 \
+  --decay-epochs 2.4 \
+  --decay-rate .97 \
+  --opt rmsproptf \
+  --opt-eps .001 \
+  -j 8 \
+  --warmup-lr 1e-6 \
+  --weight-decay 1e-5 \
+  --drop 0.3 \
+  --drop-path 0.2 \
+  --model-ema \
+  --model-ema-decay 0.9999 \
+  --aa rand-m9-mstd0.5 \
+  --remode pixel \
+  --reprob 0.2 \
+  --amp \
+  --lr .016 \
+  2>&1 | tee sdaa.log
+
+  # 生成loss对比图
+python loss.py --sdaa-log sdaa.log --cuda-log cuda.log
