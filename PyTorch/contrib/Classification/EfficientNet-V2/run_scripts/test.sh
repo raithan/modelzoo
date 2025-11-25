d2331cd15ce62c0a4078c8d316b93c3c75350122
wangli326 <1368816979@qq.com>
2025-11-25 20:13:45 +0800

    Update test.sh

--- PyTorch/contrib/Classification/EfficientNet-V2/run_scripts/test.sh
+++ PyTorch/contrib/Classification/EfficientNet-V2/run_scripts/test.sh
@@ -9,19 +9,19 @@ NUM_PROC=${1:-1}
 cd $script_path
 echo "Using NUM_PROC=${NUM_PROC}"
 echo "执行训练"
-torchrun --nproc_per_node=${NUM_PROC} train.py
-  --data-dir /data/teco-data/imagenet
-  --model efficientnetv2_s
-  --sched cosine
-  --epochs 2
-  --warmup-epochs 5
-  --lr 0.4
-  --reprob 0.5
-  --remode pixel
-  --batch-size 16
-  --amp
-  -j 4
-  2>&1 | tee sdaa.log
+torchrun --nproc_per_node=${NUM_PROC} train.py \
+    --data-dir /data/teco-data/imagenet \
+    --model efficientnetv2_s \
+    --sched cosine \
+    --epochs 2 \
+    --warmup-epochs 5 \
+    --lr 0.4 \
+    --reprob 0.5 \
+    --remode pixel \
+    --batch-size 16 \
+    --amp \
+    -j 4 \
+    2>&1 | tee sdaa.log
 
   # 生成loss对比图
 python loss.py --sdaa-log sdaa.log --cuda-log cuda.log
